c8a2d1db0c2e7d3af9a7537be1eab130e556e922
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-01 19:44:25 +0000

    Deploying to gh-pages from @ YuraStarchenko/goit-js-hw-09@50b83b3887dad2087e0bcee9e327e5e39f7ee03b 🚀

---
+++ 01-color-switcher.3ee58498.js
@@ -0,0 +1,2 @@
+[document.querySelector("button[data-start]"),document.querySelector("button[data-stop]"),document.querySelector("body")][0].addEventListener("click");
+//# sourceMappingURL=01-color-switcher.3ee58498.js.map
---
+++ 01-color-switcher.7dda643c.js
@@ -0,0 +1,2 @@
+({startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")}).startBtn.addEventListener("click");
+//# sourceMappingURL=01-color-switcher.7dda643c.js.map
