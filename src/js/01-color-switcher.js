50b83b3887dad2087e0bcee9e327e5e39f7ee03b
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-01 21:43:59 +0200

    +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,8 +5,16 @@
 
 //Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const refs = {
+	startBtn: document.querySelector('button[data-start]'),
+	stopBtn: document.querySelector('button[data-stop]'),
+	body: document.querySelector('body'),
+};
 
 
+
+refs.startBtn.addEventListener('click', );
