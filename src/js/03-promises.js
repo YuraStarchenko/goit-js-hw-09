4b4f889255e1319b2c4c4c2b83b98eaa4310e114
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-07 19:53:43 +0200

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,16 @@
+const refs = {
+	form: document.querySelector('form.form'),
+	firstDelay: document.querySelector('[name="delay"]'),
+	delayStep: document.querySelector('[name="step"]'),
+	amount: document.querySelector('[name="amount"]'),
+	createBtn: document.querySelector('[type="submit"]'),
+};
+
+refs.createBtn.addEventListener('click', () => {
+	
+});
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
