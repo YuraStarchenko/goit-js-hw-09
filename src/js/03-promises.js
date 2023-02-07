f8798ec322d9e9fb12c21ef28c732ad077fcd996
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-07 19:57:11 +0200

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,7 +7,7 @@ const refs = {
 };
 
 refs.createBtn.addEventListener('click', () => {
-	
+
 });
 
 
@@ -19,3 +19,12 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
