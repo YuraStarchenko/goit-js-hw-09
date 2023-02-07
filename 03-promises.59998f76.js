2c8336080de24b182344c35384591b1ce7e4503e
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-02-07 17:57:34 +0000

    Deploying to gh-pages from @ YuraStarchenko/goit-js-hw-09@f8798ec322d9e9fb12c21ef28c732ad077fcd996 🚀

--- 03-promises.59998f76.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector("form.form"),firstDelay:document.querySelector('[name="delay"]'),delayStep:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]'),createBtn:document.querySelector('[type="submit"]')}).createBtn.addEventListener("click",(()=>{}));
-//# sourceMappingURL=03-promises.59998f76.js.map
--- 03-promises.bbbe1d4b.js
+++
@@ -1,2 +0,0 @@
-(document.querySelector("form.form"),document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]'),document.querySelector('[type="submit"]')).addEventListener("click",(function(){}));
-//# sourceMappingURL=03-promises.bbbe1d4b.js.map
